97f663651d55c8408bb888dccba162f65453cb26
Einer Santana R <esantana@pkmds.com>
2016-02-05 17:01:52 -0400

    Noted storage class

--- noted.js
+++ noted.js
@@ -1,13 +1,87 @@
-  if (Notification.permission !== "granted")
-    Notification.requestPermission();
-  else {
-    var notification = new Notification('You have noted in this page!', {
-      icon: 'resources/logo.png',
-      body: "You have 2 notes in this web page!",
-    });
+function Noted () {
+  
+  this.pageId = 'https://developer.chrome.com/extensions/storage';
+  this.savedNotes = []
+  this.listeners = [];
+  var $this = this;
+  chrome.storage.onChanged.addListener(function () {
+    $this.notifyChanges("create", $this.savedNotes);
+  });
 
-    notification.onclick = function () {
-      window.open("http://stackoverflow.com/a/13328397/1269037");      
-    };
+  chrome.storage.sync.get({
+    notes:[]
+  },function(data) {
+     $this.savedNotes = data.notes;
+     $this.notifyChanges("loaded", data.notes);
+  }); 
 
+
+}
+
+Noted.prototype.getNotes = function(listener) {
+
+  if (this.savedNotes.length) {
+      listener(this.savedNotes);
   }
+  else {
+    chrome.storage.sync.get({
+      notes:[]
+    },function(data) {
+        listener(data.notes);
+    }); 
+  }
+
+    
+};
+
+
+
+Noted.prototype.initializeNoteService = function() {
+ 
+};
+
+Noted.prototype.onNotedChanges = function(listener) {
+  this.listeners.push(listener);
+};
+
+Noted.prototype.notifyChanges = function(action) {
+  for (var i = 0; i < this.listeners.length; i++) {
+      this.listeners[i](action, this.savedNotes);
+  };
+};
+
+Noted.prototype.sendMessage = function(message, timeout , click) {
+
+};
+
+Noted.prototype.addNote = function(note, arguments) {
+  this.savedNotes.push(note);
+  var $this = this;
+  chrome.storage.sync.set({
+    notes:this.savedNotes
+  }, function() {
+    $this.notifyChanges("create");
+  });
+};
+
+Noted.prototype.updateNote = function(index,note) {
+  this.savedNotes[index] = note;
+
+  var $this = this;
+  chrome.storage.sync.set({
+    notes:this.savedNotes
+  }, function() {
+    $this.notifyChanges("update");
+  });
+};
+
+Noted.prototype.removeNote = function(note) {  
+
+  for(var i = 0 ; i < this.savedNotes.length; i++)
+  {
+    if (this.savedNotes[i] == note) {
+        this.savedNotes.splice(i,1);
+        this.notifyChanges("delete")
+    }
+  }
+};
